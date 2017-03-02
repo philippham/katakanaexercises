e29403e37508c357394ec6ada7842a6c359b560a
philippham <philippham.it@gmail.com>
2017-03-01 22:53:41 -0500

    FindDuplicatesUnderGivenConstraints

---
+++ FindDuplicatesUnderGivenConstraints/FindDuplicatesUnderGivenConstraints/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FindDuplicatesUnderGivenConstraints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstArray = new[] { 1, 1, 1, 1, 1, 5, 7, 10, 20, 30 };
+            var secondArray = new[] { 1, 2, 3, 3, 3, 3, 3, 5, 9, 10 };
+
+            Console.WriteLine(FindDuplicate(firstArray));
+            Console.WriteLine(FindDuplicate(secondArray));
+
+            Console.ReadKey();
+        }
+
+        static int FindDuplicate(int[] numbers) // 10 elements
+        {
+            if (numbers[3] == numbers[4])
+                return numbers[3];
+            if (numbers[4] == numbers[5])
+                return numbers[4];
+            return numbers[5];
+        }
+    }
+}
---
+++ FindDuplicatesUnderGivenConstraints/README.txt
@@ -0,0 +1,12 @@
+A sorted array contains 6 different numbers, only 1 number is repeated five times. 
+So there are total 10 numbers in array. 
+Find the duplicate numbers using two comparisons only.
+
+Examples:
+
+Input: arr[] = {1, 1, 1, 1, 1, 5, 7, 10, 20, 30}
+Output: 1
+
+Input: arr[] = {1, 2, 3, 3, 3, 3, 3, 5, 9, 10}
+Output: 3
+Asked in Yahoo
